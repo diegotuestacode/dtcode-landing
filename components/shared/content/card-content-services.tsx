bfbe3e2cf6be638f08b33907b24f5ff2c3a8aa39
DIEGO ANDRES TUESTA PEREZ <diego.tuesta@elektra.com.mx>
2024-11-11 19:00:17 -0500

    Fix url contentCardServices

--- components/shared/content/card-content-services.tsx
+++ components/shared/content/card-content-services.tsx
@@ -8,10 +8,18 @@ interface ContentInterface{
     index:number;
 }
 
-const CardContentServices = async({data, host, index}: ContentInterface) => {
+const CardContentServices = async({data, index}: ContentInterface) => {
 
     const dataListPage = await getDataListCategory(data?.name);
     const { data: dataListPage1 }: { data: ItemListInterface[] } = dataListPage;
+
+    const IMG_DESARROLLO = '/assets/images/settins-icon.png'
+    const IMG_DISENO = '/assets/images/diseno-icon.png'
+    const IMG_LICENCIAS = '/assets/images/licencia-icon.png'
+    const nameService = data?.name.toLowerCase();
+    const IMG_OFICIAL =  nameService.includes("desarrollo")  ? IMG_DESARROLLO : 
+                            nameService.includes("dise") ? IMG_DISENO : 
+                            nameService.includes("licencia") ? IMG_LICENCIAS : ''
 
     return (
         <article className="flex flex-col gap-4 w-[90%] mx-auto">
@@ -20,8 +28,8 @@ const CardContentServices = async({data, host, index}: ContentInterface) => {
                 <p className="text-colorBlack2 text-center font-medium">{data.descriptionDetail}</p>
             </div>
             <div className={`flex w-full gap-10 justify-center ${(index+1) % 2 === 0 ? 'md:flex-row-reverse' : ''}`}>
-                <div className="hidden md:flex md:w-[40%] md:justify-center md:items-center ">
-                    <img className="w-[200px] bg-cover  h-[100px] rounded-md" src={host+data.image.url} alt={`Imagen del servicio de ${data.name}`} />
+                <div className="hidden md:flex md:w-[10%] md:justify-center md:items-center p-6 rounded-full bg-colorPrimary ">
+                    <img className="w-full bg-cover  h-full rounded-md" src={IMG_OFICIAL} alt={`Imagen del servicio de ${data.name}`} />
                 </div>
                 <div className="flex flex-col gap-1 w-full md:w-[60%]">
                     {
